a1d9ae644143f961d2a6a76018e2fd68d1fe69f6
Preetham Pemmasani <75422607+ppmpreetham@users.noreply.github.com>
2024-06-02 16:11:52 +0530

    Create center_select.tsx

---
+++ center_select.tsx
@@ -0,0 +1,19 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+const GsapStagger = () => {
+  useGSAP(() => {
+    gsap.to(".stagger-box", {
+      y:-25,
+      duration:1,
+      stagger:0.5,
+      ease:"back.out",
+      repeat: -1,
+      yoyo: true,
+      stagger: {
+        ease: "back.out",
+        amount: 1,
+        from: "center",
+      }
+    })
+  });
